eae4ab047b028b0cd66fa867036f169b6b9c278c
evolve2122@gmail.com <evolve2122@gmail.com>
2022-11-14 09:53:38 -0600

    Test 1, 2

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,21 @@
+int add(int a, int b)
+{
+  return a + b;
+}
+
+int sub(int a, int b)
+{
+  return a - b;
+}
+
+String showFunc(int a, int b)
+{
+	String str = "Add ${a} + ${b} = ${add(a, b)}\nSub ${a} - ${b} = ${sub(a, b)}";
+	return str;
+}
+
+void main(List<String> args) {
+  var a = int.parse(args[0]);
+  var b = int.parse(args[1]);
+  print(showFunc(a, b));
+}
