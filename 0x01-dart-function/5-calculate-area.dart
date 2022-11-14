fb9703fc7625e71129dc19e0b973630d78daf678
evolve2122@gmail.com <evolve2122@gmail.com>
2022-11-14 09:26:19 -0600

    Calculated the area of a triangle

---
+++ 0x01-dart-function/5-calculate-area.dart
@@ -0,0 +1,9 @@
+double calculateArea(double height, double base) {
+  return height * base / 2;
+}
+
+main() {
+  print(calculateArea(20,7));
+  print(calculateArea(10,7));
+  print(calculateArea(8.5,3.7));
+}
