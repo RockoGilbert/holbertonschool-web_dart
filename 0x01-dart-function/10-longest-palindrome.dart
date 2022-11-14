143c74506333131a3d8997e5be79146b4daab915
evolve2122@gmail.com <evolve2122@gmail.com>
2022-11-14 09:50:33 -0600

    long A8716 palindrome

--- 0x01-dart-function/10-longest-palindrome.dart
+++ 0x01-dart-function/10-longest-palindrome.dart
@@ -1,17 +1,23 @@
 import '9-palindrome.dart';
 
-String longestPalindrome(String str) {
+String longestPalindrome(String s) {
+  // If the length of the input string is less than 3 characters it doesn’t count as a palindrome.
+  if (s.length < 3) {
+    return 'none';
+  }
+  // If the input string is equal to its reverse, it’s a palindrome.
+  if (s == s.split('').reversed.join('')) {
+    return s;
+  }
   String longest = '';
   String current = '';
-
-  for (int i = 0; i < str.length; i++) {
-    for (int j = i; j < str.length; j++) {
-      current = str.substring(i, j + 1);
+  for (int i = 0; i < s.length; i++) {
+    for (int j = i; j < s.length; j++) {
+      current = s.substring(i, j + 1);
       if (current.length > longest.length && isPalindrome(current)) {
         longest = current;
       }
     }
   }
-
-  return longest == '' ? 'none' : longest;
+  return longest != '' ? longest : 'none';
 }
