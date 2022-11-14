59231d22e4597101d6d8853413cd13b9e30061ba
evolve2122@gmail.com <evolve2122@gmail.com>
2022-11-14 13:23:00 -0600

    3-private_password.dart

---
+++ 0x02-dart-classes/3-private_password.dart
@@ -0,0 +1,26 @@
+class Password {
+  String password = '';
+
+  Password({required this.password});
+
+  isValid() {
+    return (password.length >= 8 &&
+        password.length <= 16 &&
+        password.contains(RegExp(r'[A-Z]')) &&
+        password.contains(RegExp(r'[0-9]')));
+  }
+
+  @override
+  String toString() {
+    return 'Your Password is: $password';
+  }
+}
+
+void main() {
+  final ps = Password(password: "Passwordcode");
+  print(ps.toString());
+  print(ps.isValid());
+  final ps2 = Password(password: "PasswordDecode3");
+  print(ps2.toString());
+  print(ps2.isValid());
+}
