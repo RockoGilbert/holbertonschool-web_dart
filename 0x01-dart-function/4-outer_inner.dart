40802d4442af9b824d168b6505a3c7b7e39d99c5
evolve2122@gmail.com <evolve2122@gmail.com>
2022-11-14 09:23:07 -0600

    Wrote 2 functions and printed their output inside another

---
+++ 0x01-dart-function/4-outer_inner.dart
@@ -0,0 +1,14 @@
+void outer(String name, String id) {
+  String inner(String name, String id) {
+    String firstname = name.split(" ")[0];
+    String lastname =
+        name.substring(name.indexOf(" ") + 1, name.indexOf(" ") + 2);
+    return "Hello Agent $lastname.$firstname your id is $id";
+  }
+
+  print(inner(name, id));
+}
+
+void main() {
+  outer("Youssef Belhadj", "001");
+}
